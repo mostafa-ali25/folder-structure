c32e105c6b0a684d7dd60172914fbffec43f7581
mostafa-ali25 <mostafa.ali@arabot.io>
2025-04-14 06:41:19 +0200

    add features folder

--- src/app/(landing-page)/page.tsx
+++ src/app/(landing-page)/page.tsx
@@ -13,3 +13,15 @@ GET: Top used Agents
 */
 
 // footer (community card)
+
+import React from 'react';
+
+const LandingPage = () => {
+  return (
+    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-extrabold">Welcome to Our LandingPage</h1>
+    </div>
+  );
+};
+
+export default LandingPage;
